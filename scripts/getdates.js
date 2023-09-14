d9c778a87a7f5957b603de478f93bfeb5fc0258f
LisaMandarin <chu23001@byui.edu>
2023-09-15 03:35:23 +0800

    W01 assignment done

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+var currentYear = new Date().getFullYear();
+var yearElement = document.getElementById('year')
+yearElement.textContent = currentYear
+
+var dateTime = document.lastModified
+var dateTimeElement = document.getElementById('lastModified')
+dateTimeElement.textContent = "Last Modification: " + dateTime;
