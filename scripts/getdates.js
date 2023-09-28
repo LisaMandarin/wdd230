82aebb70ea2886b5c2aab08782cc2fc12c01e6fa
LisaMandarin <chu23001@byui.edu>
2023-09-28 08:36:55 +0800

    W03 learning activity done

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,11 @@ yearElement.textContent = currentYear
 var dateTime = document.lastModified
 var dateTimeElement = document.getElementById('lastModified')
 dateTimeElement.textContent = "Last Modification: " + dateTime;
+
+const hamButton = document.querySelector('#menu')
+const navigation = document.querySelector('.navigation')
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+})
