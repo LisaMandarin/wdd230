191cc01f2d85f973ae6049e44827a365500c5d83
LisaMandarin <chu23001@byui.edu>
2023-10-19 22:44:51 +0800

    W06 assignment done

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,17 @@
+const windChill = document.querySelector('#windchill');
+let t = 85;
+let v = 6.710;
+let floatT = parseFloat(t);
+let floatV = parseFloat(v);
+
+function windChillFormula(temp, windspeed) {
+    return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16));
+}
+
+if (floatT > 51 || floatV <= 3) {
+    windChill.textContent = "Wind Chill: " + "N/A";
+} else {
+    let result = windChillFormula(floatT, floatV);
+    windChill.textContent = "Wind Chill: " + result.toFixed(2);
+}
+
