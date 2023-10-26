8335a6818efaf8b0c8349855b33bc5703c14ab45
LisaMandarin <chu23001@byui.edu>
2023-10-26 08:42:25 +0800

    done

--- lesson03/scripts/base.js
+++ lesson03/scripts/base.js
@@ -1,3 +1,7 @@
 var year = new Date().getFullYear();
 var yearElement = document.getElementById('year');
 yearElement.textContent = year;
+
+var lastModified = new Date(document.lastModified);
+var lastModifiedElement = document.getElementById('lastModified');
+lastModifiedElement.textContent = lastModified;
