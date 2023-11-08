255384a3c095bf0a4cb0966c1d9c37af5af86188
LisaMandarin <chu23001@byui.edu>
2023-11-08 13:41:53 +0800

    JSON Fetch API done

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,20 +1,18 @@
-const kp1 = document.querySelector("#keyphrase");
-const kp2 = document.querySelector("#keyphrase2");
-const message = document.querySelector("#formmessage");
-
-kp2.addEventListener("focusout", checkSame);
-
-// This should be refactored.
-function checkSame() {
-	if (kp1.value !== kp2.value) {
-		message.textContent = "❗Key Phrases DO NOT MATCH!";
-		message.style.visibility = "show";
-		kp2.style.backgroundColor = "#fff0f3";
-		kp2.value = "";
-		kp2.focus();
-	} else {
-		message.style.display = "none";
-		kp2.style.backgroundColor = "#fff";
-		kp2.style.color = "#000";
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+let results = null;
+async function getPokemon(url) {
+	const results = await fetch(url);
+	if (results.ok) {
+		const data = await results.json();
+		doStuff(data);
 	}
 }
+
+function doStuff(data) {
+	results = data;
+	console.log("first: ", results)
+}
+
+
+getPokemon(url);
+console.log("second: ", results)
