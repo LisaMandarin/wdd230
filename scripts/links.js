bbee2102e83da98622d95341fdf3dd7451eb0943
LisaMandarin <chu23001@byui.edu>
2023-11-08 19:41:59 +0800

    JSON Menu Done

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,28 @@
 const baseURL = "https://lisamandarin.github.io/wdd230/";
 const linksURL = "https://lisamandarin.github.io/wdd230/data/links.json";
+const lessonList = document.querySelector('#lesson-list');
 
 async function getLinks() {
     const response = await fetch(linksURL);
-    const data = response.json();
-    console.log(data);
+    const data = await response.json();
+    // console.table(data.lessons);
+    // console.log(data);
+    displayLinks(data.lessons);
 }
 
+function displayLinks(weeks) {
+    weeks.forEach((week) => {
+        let list = document.createElement('li');
+        list.textContent = `${week.lesson}: `
+
+        week.links.forEach((linkData) => {
+            let link = document.createElement('a');
+            link.setAttribute('href', baseURL + linkData.url);
+            link.textContent = `${linkData.title} | `;
+            list.appendChild(link);
+        });
+        
+        lessonList.appendChild(list);
+    });
+}
 getLinks();
