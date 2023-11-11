0b277c15b46cf11faef1fd0da6278a25731ae3d9
LisaMandarin <chu23001@byui.edu>
2023-11-12 02:57:35 +0800

    fix dark mode

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -5,7 +5,8 @@ const main = document.querySelector('main');
 const darkLight = document.querySelectorAll('.dark-light');
 const thankyouH1 = document.querySelectorAll('.thankyou-h1');
 const directoryContainer = document.querySelector('#directory-container');
-const directoryNames = document.querySelectorAll('#directory-container .directoryname');
+const directoryNames = document.querySelectorAll('#directory-container .directory-name');
+const directoryNamesAnchor = document.querySelectorAll('.company-name');
 
 
 modeButton.addEventListener('click', () => {
@@ -13,22 +14,31 @@ modeButton.addEventListener('click', () => {
         cardSmall.forEach((card) => {
             card.style.background = '#2D3142';
             card.style.color = '#f8f8f8'
+            alert(1);
         })
         discoverCard.forEach((card) => {
             card.style.background = '#2D3142'
             card.style.color = '#f8f8f8';
+            alert(2);
         });
         main.style.backgroundColor = "#2D3142";
         darkLight.forEach((word) => {
             word.style.color = "#f8f8f8";
+            alert(3);
         });
         thankyouH1.forEach((word)=> {
             word.style.color = "#f8f8f8";
+            alert(4);
         });
         directoryContainer.style.color = '#f8f8f8';
-        directoryNames.forEach((directoryLink) => {
-            directoryLink.style.color = "#f8f8f8";
+        directoryNames.forEach((directoryNames) => {
+            directoryNames.style.color = "#f8f8f8";
+            alert(5);
         });
+        directoryNamesAnchor.forEach((directoryNamesAnchor) => {
+            directoryNamesAnchor.style.color = "red";
+            alert(6);
+        })
         modeButton.textContent = "🔆";
     }
     else {
@@ -48,8 +58,8 @@ modeButton.addEventListener('click', () => {
             word.style.color = "#2D3142";
         });
         directoryContainer.style.color = '#2D3142';
-        directoryNames.forEach((directoryLink) => {
-            directoryLink.style.color = "#2D3142";
+        directoryNames.forEach((directoryNames) => {
+            directoryNames.style.color = "#2D3142";
         });
         modeButton.textContent = "🕶️";
     }
