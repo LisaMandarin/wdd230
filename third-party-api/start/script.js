e3f90a217fdbf1934b966ec8b88c04551452eb2c
LisaMandarin <chu23001@byui.edu>
2023-11-15 22:42:48 +0800

    weather API done

---
+++ third-party-api/start/script.js
@@ -0,0 +1,26 @@
+L.mapquest.key = 'oVI2XMSe0MUGj1NkBpAQTTyBKJHN3Q9X';
+
+// 'map' refers to a <div> element with the ID map
+const map = L.mapquest.map('map', {
+  center: [53.480759, -2.242631],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
+// map.addControl(L.mapquest.control());
+map.addControl(L.mapquest.control({ position: "bottomright" }));
+
+window.onload = function() {
+  L.marker([53.480759, -2.242631], {
+    icon: L.mapquest.icons.marker({
+      primaryColor: "#22407F",
+      secondaryColor: "#3B5998",
+      shadow: true,
+      size: "md",
+      symbol: "A",
+    }),
+  })
+    .bindPopup("This is Manchester!")
+    .addTo(map);
+}
+
+
