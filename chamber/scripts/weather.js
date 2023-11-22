c59deaae1218a546733e8614cd466d98b0efedd5
LisaMandarin <chu23001@byui.edu>
2023-11-23 04:20:16 +0800

    current weather container fixed

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,52 +1,30 @@
-const currentTemperature = document.querySelector('#current-temperature');
-const currentWeather = document.querySelector('#current-weather');
 const weatherForcast = document.querySelector('#weather-forcast');
-
 const forcastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=22.67&lon=120.31&units=metric&appid=6df76dc2dca1a837e79bfff8b684afe4';
-const currentURL = 'https://api.openweathermap.org/data/2.5/weather?lat=22.67&lon=120.31&units=metric&appid=6df76dc2dca1a837e79bfff8b684afe4';
-
-async function apiFetchCurrent() {
-    try {
-        const response = await fetch(currentURL);
-        if (response.ok) {
-            const data = await response.json();
-            // console.log(data);
-            const temp = data.main.temp
-            const description = data.weather[0].description
-            // console.log(temp);
-            // console.log(description);
-            currentTemperature.textContent = `Current Temperaturent: ${temp}℃`;
-            currentWeather.textContent = `Current Weather: ${description.toUpperCase()}`;
-        } else {
-            throw Error (await response.text());
-        }
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
 
 async function apiFetchForcast() {
     try {
         const response = await fetch(forcastURL);
         if (response.ok) {
             const data = await response.json();
-
-            const temp1 = data.list[8].main.temp;
+            console.log(data);
+            let temp1 = data.list[8].main.temp;
+            temp1 = Math.round(temp1);
             const timeString1 = data.list[8].dt_txt;
             const day1 = getReadableDay(timeString1);
             const date1 = getReadableDate(timeString1);
             
             styleForcast(day1, date1, temp1);
 
-            const temp2 = data.list[16].main.temp;
+            let temp2 = data.list[16].main.temp;
+            temp2 = Math.round(temp2);
             const timeString2 = data.list[16].dt_txt;
             const day2 = getReadableDay(timeString2);
             const date2 = getReadableDate(timeString2);
             
             styleForcast(day2, date2, temp2);
 
-            const temp3 = data.list[24].main.temp;
+            let temp3 = data.list[24].main.temp;
+            temp3 = Math.round(temp3);  
             const timeString3 = data.list[24].dt_txt;
             const day3 = getReadableDay(timeString3);
             const date3 = getReadableDate(timeString3);
@@ -79,26 +57,30 @@ function getReadableDay(timeString) {
 
 function styleForcast(day, date, temperature) {
     const div = document.createElement('div')
-    const h3 = document.createElement('h2');
-    const p1 = document.createElement('p');
-    const p2 = document.createElement('p');
-    h3.innerHTML = day;
-    p1.textContent = date;
-    p2.textContent = `${temperature}℃`;
-    h3.style.background = "none";
-    h3.style.color = "#EF8354";
-    p1.style.textAlign = "center";
-    p1.style.padding = "0";
-    p2.style.textAlign = "center";
-    p2.style.fontSize = "2rem";
+    const h3date = document.createElement('h3');
+    const h3day = document.createElement('h3');
+    const p = document.createElement('p');
+    h3date.textContent = date;
+    h3day.textContent = day;
+    p.textContent = `${temperature}℃`;
+    h3date.style.marginBottom = "0";
+    h3date.style.fontSize = "2rem";
+    h3day.style.marginTop = "0";
+    p.style.padding = "0";
+    p.style.fontSize = "1.5rem";
     div.style.boxSizing = "border-box";
+    div.style.textAlign = "center";
+    div.style.border = "2px solid #2D3142";
+    div.style.boxShadow = "5px 5px 5px #2D3142";
+    div.style.padding = "10px";
+    div.style.margin = "5px";
     weatherForcast.style.boxSizing = "border-box";
     
-    div.appendChild(h3);
-    div.appendChild(p1);
-    div.appendChild(p2);
+    div.appendChild(h3date);
+    div.appendChild(h3day);
+    div.appendChild(p);
 
     weatherForcast.appendChild(div);
 }
-apiFetchCurrent();
+
 apiFetchForcast();
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,23 +1,55 @@
+const currentTemperature = document.querySelector('#current-temperature');
+const windSpeed = document.querySelector('#wind-speed')
 const windChill = document.querySelector('#windchill');
-let t = 85;
-let v = 6.710;
-let floatT = parseFloat(t);
-let floatV = parseFloat(v);
+const currentWeather = document.querySelector('#current-weather');
+const weatherIcon = document.querySelector('#weather-icon');
+
+const currentURL = 'https://api.openweathermap.org/data/2.5/weather?lat=22.67&lon=120.31&units=metric&appid=6df76dc2dca1a837e79bfff8b684afe4';
+
+async function apiFetchCurrent() {
+    try {
+        const response = await fetch(currentURL);
+        if (response.ok) {
+            const data = await response.json();
+            let temp = data.main.temp;
+            temp = Math.round(temp);
+            const v = data.wind.speed;
+            const description = data.weather[0].description
+            const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+            
+            currentTemperature.textContent = `${temp}℃`;
+            currentTemperature.style.fontSize = "1.7rem";
+            windSpeed.textContent = `${v} mph`;
+            windSpeed.style.fontSize = "1.5rem";
+            showWindChill(temp, v);
+            currentWeather.textContent = `${description.toUpperCase()}`;
+            let weatherImage = document.createElement('img');
+            weatherImage.setAttribute('src', iconsrc);
+            weatherImage.setAttribute('alt', description);
+            weatherIcon.appendChild(weatherImage);
+
+            
+        } else {
+            throw Error (await response.text());
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
 
 function windChillFormula(temp, windspeed) {
     return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16));
 };
 
-const temperature = document.querySelector('#temperature');
-const windSpeed = document.querySelector('#wind-speed')
-
-temperature.textContent = "Temperature: " + t + " °F";
-windSpeed.textContent = "Wind Speed: " + v + " mph";
-
-if (floatT > 51 || floatV <= 3) {
-    windChill.textContent = "Wind Chill: " + "N/A";
-} else {
-    let result = windChillFormula(floatT, floatV);
-    windChill.textContent = "Wind Chill: " + result.toFixed(2);
+function showWindChill(temp, windspeed) {
+    if (temp > 51 || windspeed <= 3) {
+        windChill.textContent = "N/A";
+    } else {
+        let result = windChillFormula(floatT, floatV);
+        windChill.textContent = result.toFixed(2);
+        
+    }
 }
 
+apiFetchCurrent();
