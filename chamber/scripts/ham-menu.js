e8323143eb5979fef309cc169ffcc3394f9f9e9e
LisaMandarin <chu23001@byui.edu>
2023-11-24 21:19:36 +0800

    hamburger menu and dark mode positins fixed

--- chamber/scripts/ham-menu.js
+++ chamber/scripts/ham-menu.js
@@ -1,5 +1,6 @@
 const hamButtonElement = document.querySelector('#menu');
 const navigationElement = document.querySelector('.navigation')
+const slideUp = document.querySelector('#slide-up');
 
 hamButtonElement.addEventListener('click', () => {
     navigationElement.classList.toggle('open');
