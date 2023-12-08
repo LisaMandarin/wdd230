34e718df720053d3dee645dbafed22025b1bf408
LisaMandarin <chu23001@byui.edu>
2023-12-09 01:50:20 +0800

    json done

--- scoots/scripts/rentals.js
+++ scoots/scripts/rentals.js
@@ -1,1 +1,21 @@
 const url = 'https://lisamandarin.github.io/wdd230/scoots/data/rentals.json';
+const container = document.querySelector('#rentals-description')
+
+async function getData() {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+    }
+}
+
+function formatData(type) {
+    const sectionElement = document.createElement('section');
+    const nameElement = document.createElement('h1');
+
+    nameElement.textContent = type;
+    sectionElement.appendChild(nameElement);
+    container.appendChild(sectionElement);
+}
+
+getData();
