6066847088426f99c44405909f90a9a78d68089c
LisaMandarin <chu23001@byui.edu>
2023-12-08 19:58:44 +0800

    hamburger menu text fixed

--- scoots/scripts/open-menu.js
+++ scoots/scripts/open-menu.js
@@ -1,8 +1,13 @@
 const menuIcon = document.querySelector('#menu-icon');
+const menuIconSmall = document.querySelector('#menu-icon-small-screen');
 const navigation = document.querySelector('#sub-header nav');
 const menuCloseButton = document.querySelector('#menu-close-button');
 
 menuIcon.addEventListener('click', () => {
+    navigation.classList.toggle('open-menu');
+})
+
+menuIconSmall.addEventListener('click', () => {
     navigation.classList.toggle('open-menu');
 })
 
