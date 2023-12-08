d2810c9645219e9f1f409f297a4a55c588b52272
LisaMandarin <chu23001@byui.edu>
2023-12-09 03:17:33 +0800

    JSON file fixed

--- scoots/scripts/rentals.js
+++ scoots/scripts/rentals.js
@@ -1,24 +1,38 @@
-const url = 'https://lisamandarin.github.io/wdd230/scoots/data/vehicles.json';
-const container = document.querySelector('#rentals-description')
+    const url = 'https://lisamandarin.github.io/wdd230/scoots/data/vehicles.json';
+    const container = document.querySelector('#rentals-description')
 
-async function getData() {
-    const response = await fetch(url);
-    if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        data.forEach((item) => { 
-            console.log(item);
-        });
+    async function getData() {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            const vehicles = data.vehicles;
+            vehicles.forEach((vehicle) => {
+                const type = vehicle.type;
+                // formatData(type)
+                
+                const descriptions = vehicle.description;
+                descriptions.forEach((description) => {
+                    const name = description.name;
+                    const number = description.number;
+                    const resvFull = description.resvFull;
+                    const resvHalf = description.resvHalf;
+                    console.log(resvHalf);
+                    console.log(resvFull);
+                    console.log(name);
+                    console.log(number);
+                })
+            })
+        }
     }
-}
 
-function formatData(type) {
-    const sectionElement = document.createElement('section');
-    const nameElement = document.createElement('h1');
+    // function formatData(type) {
+    //     const sectionElement = document.createElement('section');
+    //     const nameElement = document.createElement('h1');
 
-    nameElement.textContent = type;
-    sectionElement.appendChild(nameElement);
-    container.appendChild(sectionElement);
-}
+    //     nameElement.textContent = type;
+    //     sectionElement.appendChild(nameElement);
+    //     container.appendChild(sectionElement);
+    // }
 
-getData();
+    getData();
