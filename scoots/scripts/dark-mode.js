cffd7b66ddb594591bae2b3d3d80f8c90bef619e
LisaMandarin <chu23001@byui.edu>
2023-12-12 00:18:11 +0800

    index.html fixed

--- scoots/scripts/dark-mode.js
+++ scoots/scripts/dark-mode.js
@@ -4,7 +4,7 @@ const switchElement = document.querySelector('#dark');
 switchElement.addEventListener('change', () => {
     if (switchElement.checked) {
         document.body.style.backgroundColor = 'black';
-        document.body.style.color = 'white';
+        document.body.style.color = '#F2E8CF';
     } else {
         document.body.style.backgroundColor = '';
         document.body.style.color = '';
