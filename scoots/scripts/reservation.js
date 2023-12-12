76bebc015f7888f2b9f073e97b73ae03e17449f2
LisaMandarin <chu23001@byui.edu>
2023-12-12 23:49:06 +0800

    reservation.html checked

--- scoots/scripts/reservation.js
+++ scoots/scripts/reservation.js
@@ -1,16 +1,17 @@
 const quantity = document.querySelector('#form-quantity');
 const quantityAlert = document.querySelector('#quantity-alert');
 
+// -----quantity-----
 quantity.addEventListener('keyup', () => checkQuantityRule(quantity.value));
 
 function checkQuantityRule(input) {
     const regEx = /^[1-5]$/;
     if (regEx.test(input)) {
         quantityAlert.style.display = "none";
-        console.log(input + "right")
     } else {
         quantityAlert.style.display = "block";
         quantityAlert.textContent = "Enter number 1 to 5";
         quantityAlert.style.color = "red";
     }
 }
+
